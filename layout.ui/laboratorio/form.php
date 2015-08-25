78bcc60690f57c674bbae14d200360c53f4edf7e
jmverrelli <juanm.verrelli@gmail.com>
2015-08-25 20:51:15 -0300

    Formulario Laboratorio
    Agrego vista basica del formulario de laboratorio en layout.ui

---
+++ layout.ui/laboratorio/form.php
@@ -0,0 +1,95 @@
+<h3>Sangre</h3>
+<hr>
+<table>
+<tr>
+<td><label for='hto'>Hto</label></td>
+<td><label for='hb'>Hb</label></td>    
+<td><label for='gblancos'>G. Blancos</label></td>
+<td><label for='plaq'>Plaq</label></td>
+<td><label for='glucemia'>Glucemia</label></td>
+<td><label for='urea'>Urea</label></td>
+<td><label for='creat'>Creat</label></td>
+<td><label for='na'>Na</label></td>
+<td><label for='k'>K</label></td>
+<td><label for='lactico'>Lactico</label></td>
+</tr>
+<tr>
+<td><input type='text' id='hto' /></td>
+<td><input type='text' id='hb' /></td>
+<td><input type='text' id='gblancos' /></td>
+<td><input type='text' id='plaq' /></td>
+<td><input type='text' id='glucemia' /></td>
+<td><input type='text' id='urea' /></td>
+<td><input type='text' id='creat' /></td>
+<td><input type='text' id='na' /></td>
+<td><input type='text' id='k' /></td>
+<td><input type='text' id='lactico' /></td>
+</tr>
+<tr>
+<td><label for='got'>Got</label></td>
+<td><label for='gpt'>Gpt</label></td>
+<td><label for='amilasa'>Amilasa</label></td>
+<td><label for='bt'>Bt</label></td>
+<td><label for='bd'>Bd</label></td>
+<td><label for='fal'>Fal</label></td>
+<td><label for='tprot'>T Prot %</label></td>
+<td><label for='apttseg'>Aptt seg</label></td>
+</tr>
+<tr>
+<td><input type='text' id='got' /></td>
+<td><input type='text' id='gpt' /></td>
+<td><input type='text' id='amilasa' /></td>
+<td><input type='text' id='bt' /></td>
+<td><input type='text' id='bd' /></td>
+<td><input type='text' id='fal' /></td>
+<td><input type='text' id='tprot' /></td>
+<td><input type='text' id='apttseg' /></td>
+</tr>
+<tr>
+<td><label for='ph'>Ph</label></td>
+<td><label for='co2'>Co2</label></td>
+<td><label for='excbase'>Exc Base</label></td>
+<td><label for='hco3'>HcO3</label></td>
+<td><label for='po2'>Po2</label></td>
+<td><label for='saro2'>SarO2</label></td>
+<td><label for='cpk'>Cpk</label></td>
+</tr>
+<tr>
+<td><input type='text' id='ph' /></td>
+<td><input type='text' id='co2' /></td>
+<td><input type='text' id='excbase' /></td>
+<td><input type='text' id='hco3' /></td>
+<td><input type='text' id='po2' /></td>
+<td><input type='text' id='saro2' /></td>
+<td><input type='text' id='cpk' /></td>
+</tr>
+<tr>
+<td><label for='esd'>ESD</label></td>
+<td><label for='pcr'>PCR</label></td>
+<td><label for='procalcitonina'>Procalcitonina</label></td>
+</tr>
+<tr>
+<td><input type='text' id='esd' /></td>
+<td><input type='text' id='pcr' /></td>
+<td><input type='text' id='procalcitonina' /></td>
+</tr>
+</table>
+<h3>Orina</h3>
+<hr>
+<table>
+<tr>
+<td><label for='lcrorina'>LCR</label></td>
+<td><label for='gborina'>GB.</label></td>
+
+<td><label for='protorina'>Prot.</label></td>
+<td><label for='glucorina'>Gluc.</label></td>
+<td><label for='aspectoorina'>Aspecto</label></td>
+</tr>
+<tr>
+<td><input type='text' id='lcrorina' /></td>
+<td><input type='text' id='gborina' /></td>
+<td><input type='text' id='protorina' /></td>
+<td><input type='text' id='glucorina' /></td>
+<td><input type='text' id='aspectoorina' /></td>
+</tr>
+</table>
