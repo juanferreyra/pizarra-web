0bd4ed5327811c8490959a847cfe9970897c390b
Juan Martin Verrelli <jmverrelli@debianTuto.com>
2015-09-03 15:57:53 -0300

    Agrego una lista de cosas para hacer en comentarios

--- layout.ui/formulario.internacion/paciente/form.php
+++ layout.ui/formulario.internacion/paciente/form.php
@@ -1,3 +1,13 @@
+<!--
+-Agregar los formularios a la vista
+-Agregar para obtener el numero de historia clinica
+-Agregar guardados a la base
+-Cargar los datos guardados en la base en el menu de internacion
+-Agregar la cama y de que lado esta internado
+-Mostrar en la grilla las internaciones cargadas
+
+-->
+
 <h3>Datos personales paciente</h3>
 <hr>
 <form id="frmPaciente" action="../../services/paciente.service.php" method="POST">
