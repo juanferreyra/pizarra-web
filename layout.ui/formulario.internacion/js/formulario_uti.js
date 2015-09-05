56d448e44eddd5e7988af7ae5155fd65b07b104d
Juan Ferreyra <juangabrielferreyra93@gmail.com>
2015-09-05 01:01:11 -0300

    le agarro la mano a los dialogos

--- layout.ui/formulario.internacion/js/formulario_uti.js
+++ layout.ui/formulario.internacion/js/formulario_uti.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 
 	$("#laboratorio").click(function(){
-	  	$("#contenido_laboratorio").load("forms/laboratorio/form.laboratorio.php");
+	  	$("#dialog_subcontent").load("forms/laboratorio/form.laboratorio.php");
 	});
 
 	$("#paciente").click(function(){
-		$("#contenido_paciente").load("forms/paciente/form.paciente.php");
+		$("#dialog_subcontent2").load("forms/paciente/form.paciente.php");
 	});
 
 	$("#paciente_peso").click(function(){
-		$("#contenido_paciente_peso").load("forms/paciente/form.peso.php");
+		$("#dialog_subcontent3").load("forms/paciente/form.peso.php");
 	});
 
 	$("#TET").click(function(){
-		$("#contenido_tet").load("forms/paciente/form.tac.php");
+		$("#dialog_subcontent4").load("forms/paciente/form.tac.php");
 	});
 
 });
